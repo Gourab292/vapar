9bc6da4b7a9a944adf8881e0169e22c93e39ff22
IamShakal <gourabdas2927@gmail.com>
2020-11-01 09:13:21 +0530

    Updates

---
+++ precache-manifest.ea66882f139fe53c5147c1c80834479b.js
@@ -0,0 +1,558 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "559e16b48b2e8b94db00174382a3e38c",
+    "url": "/Gourab292.github.io/vapar/index.html"
+  },
+  {
+    "revision": "082e6cd192a8e90ed019",
+    "url": "/Gourab292.github.io/vapar/static/css/0.3fc7a7e5.chunk.css"
+  },
+  {
+    "revision": "6a96884b8735da1caa82",
+    "url": "/Gourab292.github.io/vapar/static/css/5.b46d83e5.chunk.css"
+  },
+  {
+    "revision": "0e660a2db204477bb709",
+    "url": "/Gourab292.github.io/vapar/static/css/main.9f5d4deb.chunk.css"
+  },
+  {
+    "revision": "082e6cd192a8e90ed019",
+    "url": "/Gourab292.github.io/vapar/static/js/0.30e19712.chunk.js"
+  },
+  {
+    "revision": "ba19815fdf5b3bd74b59",
+    "url": "/Gourab292.github.io/vapar/static/js/3.a8e0e392.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/Gourab292.github.io/vapar/static/js/3.a8e0e392.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ea6171de71024eba715",
+    "url": "/Gourab292.github.io/vapar/static/js/4.4a83c7f0.chunk.js"
+  },
+  {
+    "revision": "6a96884b8735da1caa82",
+    "url": "/Gourab292.github.io/vapar/static/js/5.5608dc4c.chunk.js"
+  },
+  {
+    "revision": "fb0c55ac995015012a6a",
+    "url": "/Gourab292.github.io/vapar/static/js/6.cc209387.chunk.js"
+  },
+  {
+    "revision": "a9b47b6de8b7ed18475b",
+    "url": "/Gourab292.github.io/vapar/static/js/7.f13829f7.chunk.js"
+  },
+  {
+    "revision": "0e660a2db204477bb709",
+    "url": "/Gourab292.github.io/vapar/static/js/main.109ff703.chunk.js"
+  },
+  {
+    "revision": "354c20505d90d067f628",
+    "url": "/Gourab292.github.io/vapar/static/js/runtime-main.4ec667f0.js"
+  },
+  {
+    "revision": "58b45d84f30bbf14a333b9f0b0d561f3",
+    "url": "/Gourab292.github.io/vapar/static/media/5-1.58b45d84.svg"
+  },
+  {
+    "revision": "27b61bd1bffc682fe8501f804f81beba",
+    "url": "/Gourab292.github.io/vapar/static/media/5-2.27b61bd1.svg"
+  },
+  {
+    "revision": "7e28f503bdbcbbe1cb0b01f7c9c8de00",
+    "url": "/Gourab292.github.io/vapar/static/media/5-3.7e28f503.svg"
+  },
+  {
+    "revision": "354b71f61e95add9460382612d12ea84",
+    "url": "/Gourab292.github.io/vapar/static/media/HKGrotesk-Bold.354b71f6.woff2"
+  },
+  {
+    "revision": "39abef2784242edc2ee35a494913921a",
+    "url": "/Gourab292.github.io/vapar/static/media/HKGrotesk-Bold.39abef27.woff"
+  },
+  {
+    "revision": "34870e1ed66705a49ee4bfe3b9788446",
+    "url": "/Gourab292.github.io/vapar/static/media/HKGrotesk-BoldItalic.34870e1e.woff2"
+  },
+  {
+    "revision": "e2ccafd320ad85fa2234196b29b4f815",
+    "url": "/Gourab292.github.io/vapar/static/media/HKGrotesk-BoldItalic.e2ccafd3.woff"
+  },
+  {
+    "revision": "a29528e2152bd934e80c957b4202ada4",
+    "url": "/Gourab292.github.io/vapar/static/media/HKGrotesk-Italic.a29528e2.woff2"
+  },
+  {
+    "revision": "d20b5c1b8a2a79ac879d77401b3332dd",
+    "url": "/Gourab292.github.io/vapar/static/media/HKGrotesk-Italic.d20b5c1b.woff"
+  },
+  {
+    "revision": "010590af7a92baf90289e16b38455dec",
+    "url": "/Gourab292.github.io/vapar/static/media/HKGrotesk-Light.010590af.woff2"
+  },
+  {
+    "revision": "f732674ffee99683f7ecaf200304fc2f",
+    "url": "/Gourab292.github.io/vapar/static/media/HKGrotesk-Light.f732674f.woff"
+  },
+  {
+    "revision": "16feef0468c1b21da97828bc7f247d34",
+    "url": "/Gourab292.github.io/vapar/static/media/HKGrotesk-LightItalic.16feef04.woff"
+  },
+  {
+    "revision": "ee0c69792f73146c22766e56462e0b18",
+    "url": "/Gourab292.github.io/vapar/static/media/HKGrotesk-LightItalic.ee0c6979.woff2"
+  },
+  {
+    "revision": "2322beeac10f60c7eb4e7f1b6a8be0fe",
+    "url": "/Gourab292.github.io/vapar/static/media/HKGrotesk-Medium.2322beea.woff2"
+  },
+  {
+    "revision": "302a18585d206b228f371d69b0568e9b",
+    "url": "/Gourab292.github.io/vapar/static/media/HKGrotesk-Medium.302a1858.woff"
+  },
+  {
+    "revision": "3851d1ee4964c1c0a9b4eacd197791f2",
+    "url": "/Gourab292.github.io/vapar/static/media/HKGrotesk-MediumItalic.3851d1ee.woff"
+  },
+  {
+    "revision": "69f417a1d933c4e77f2b38b60dd5df91",
+    "url": "/Gourab292.github.io/vapar/static/media/HKGrotesk-MediumItalic.69f417a1.woff2"
+  },
+  {
+    "revision": "923012e8df3ecf9c62a9bb65c9dc7c20",
+    "url": "/Gourab292.github.io/vapar/static/media/HKGrotesk-Regular.923012e8.woff"
+  },
+  {
+    "revision": "a67cda8292bc244b2022197d77911c45",
+    "url": "/Gourab292.github.io/vapar/static/media/HKGrotesk-Regular.a67cda82.woff2"
+  },
+  {
+    "revision": "1a81aa6acd4a99a647fdffcf9ebd1238",
+    "url": "/Gourab292.github.io/vapar/static/media/HKGrotesk-SemiBold.1a81aa6a.woff2"
+  },
+  {
+    "revision": "7161e29bd42b6dacba8629bc4a8ef14a",
+    "url": "/Gourab292.github.io/vapar/static/media/HKGrotesk-SemiBold.7161e29b.woff"
+  },
+  {
+    "revision": "1533907b0cd6ca6f15f5da29d73c8a0b",
+    "url": "/Gourab292.github.io/vapar/static/media/HKGrotesk-SemiBoldItalic.1533907b.woff"
+  },
+  {
+    "revision": "b1a89fcc669c2b5a42b376b867a64437",
+    "url": "/Gourab292.github.io/vapar/static/media/HKGrotesk-SemiBoldItalic.b1a89fcc.woff2"
+  },
+  {
+    "revision": "04f8934c2328b60047ab5d3eb46b23ca",
+    "url": "/Gourab292.github.io/vapar/static/media/Jam.04f8934c.ttf"
+  },
+  {
+    "revision": "6fade5b037a76c060ea70d369b4e0abd",
+    "url": "/Gourab292.github.io/vapar/static/media/Jam.6fade5b0.svg"
+  },
+  {
+    "revision": "b6959e3ebf2d7ab313d44a7a5a40e741",
+    "url": "/Gourab292.github.io/vapar/static/media/Jam.b6959e3e.eot"
+  },
+  {
+    "revision": "fcddb1043335e251a3b3f79a1e9700ad",
+    "url": "/Gourab292.github.io/vapar/static/media/Jam.fcddb104.woff"
+  },
+  {
+    "revision": "3917a658bdefd876bd80b6f2a681057a",
+    "url": "/Gourab292.github.io/vapar/static/media/Jost-Bold.3917a658.woff"
+  },
+  {
+    "revision": "cefa0e6162788e85c28b7447d669764a",
+    "url": "/Gourab292.github.io/vapar/static/media/Jost-Bold.cefa0e61.woff2"
+  },
+  {
+    "revision": "433e12e28e9e3469bc57d1203558318e",
+    "url": "/Gourab292.github.io/vapar/static/media/Jost-BoldItalic.433e12e2.woff2"
+  },
+  {
+    "revision": "6470c485791e5c102fdf54cb399e6df0",
+    "url": "/Gourab292.github.io/vapar/static/media/Jost-BoldItalic.6470c485.woff"
+  },
+  {
+    "revision": "027526138bcc2d5ac64e6959e1ecd424",
+    "url": "/Gourab292.github.io/vapar/static/media/Jost-Book.02752613.woff"
+  },
+  {
+    "revision": "27b5323f596ca2495a15342a63566c8c",
+    "url": "/Gourab292.github.io/vapar/static/media/Jost-Book.27b5323f.woff2"
+  },
+  {
+    "revision": "193f0bbb10de66226d9596c90c436853",
+    "url": "/Gourab292.github.io/vapar/static/media/Jost-BookItalic.193f0bbb.woff"
+  },
+  {
+    "revision": "c09489e22ca66aea9c55a8bd77ae1c51",
+    "url": "/Gourab292.github.io/vapar/static/media/Jost-BookItalic.c09489e2.woff2"
+  },
+  {
+    "revision": "59ce9a68fd9ff1e510dc627b6199e3dc",
+    "url": "/Gourab292.github.io/vapar/static/media/Jost-Light.59ce9a68.woff"
+  },
+  {
+    "revision": "fe224dfd1dedeb19cde5e3f75b6a021b",
+    "url": "/Gourab292.github.io/vapar/static/media/Jost-Light.fe224dfd.woff2"
+  },
+  {
+    "revision": "41b117e3930e2d8566d9331752e6049a",
+    "url": "/Gourab292.github.io/vapar/static/media/Jost-LightItalic.41b117e3.woff"
+  },
+  {
+    "revision": "49a20184861c8320835e4228cbc1c4c6",
+    "url": "/Gourab292.github.io/vapar/static/media/Jost-LightItalic.49a20184.woff2"
+  },
+  {
+    "revision": "1bfc01b930555920e420af42c66954cf",
+    "url": "/Gourab292.github.io/vapar/static/media/Jost-Medium.1bfc01b9.woff"
+  },
+  {
+    "revision": "5aae01893cc854e9e728afea1e8e858a",
+    "url": "/Gourab292.github.io/vapar/static/media/Jost-Medium.5aae0189.woff2"
+  },
+  {
+    "revision": "0c5d4c3f3bc4b72f1584da8dcfee00a6",
+    "url": "/Gourab292.github.io/vapar/static/media/Jost-MediumItalic.0c5d4c3f.woff"
+  },
+  {
+    "revision": "9b54845cd6d64fe1ab5fe7d637aba60b",
+    "url": "/Gourab292.github.io/vapar/static/media/Jost-MediumItalic.9b54845c.woff2"
+  },
+  {
+    "revision": "24193f5b82f16ba01b8c84a3a6294efd",
+    "url": "/Gourab292.github.io/vapar/static/media/Jost-Semi.24193f5b.woff2"
+  },
+  {
+    "revision": "e69ff5a885d7ac8f633b58cf4e372f8d",
+    "url": "/Gourab292.github.io/vapar/static/media/Jost-Semi.e69ff5a8.woff"
+  },
+  {
+    "revision": "2e742fa2a0fd5613bd5d43752ae5038f",
+    "url": "/Gourab292.github.io/vapar/static/media/Jost-SemiItalic.2e742fa2.woff"
+  },
+  {
+    "revision": "ecee1d5841a3e2baa60e481278852fc0",
+    "url": "/Gourab292.github.io/vapar/static/media/Jost-SemiItalic.ecee1d58.woff2"
+  },
+  {
+    "revision": "2646113a1f5c5ca4728b05d88b276e1e",
+    "url": "/Gourab292.github.io/vapar/static/media/Museo300-Regular-webfont.2646113a.woff"
+  },
+  {
+    "revision": "bd5793996213550d5d9ea47a48e33bee",
+    "url": "/Gourab292.github.io/vapar/static/media/Museo300-Regular-webfont.bd579399.woff2"
+  },
+  {
+    "revision": "3526fda2483c6b15b2cae656a560570a",
+    "url": "/Gourab292.github.io/vapar/static/media/Museo500-Regular-webfont.3526fda2.woff"
+  },
+  {
+    "revision": "e1bd310cba93273cde66f9bcae70faf6",
+    "url": "/Gourab292.github.io/vapar/static/media/Museo500-Regular-webfont.e1bd310c.woff2"
+  },
+  {
+    "revision": "6832e2663b70d1950a549140ac496ef1",
+    "url": "/Gourab292.github.io/vapar/static/media/Museo700-Regular-webfont.6832e266.woff2"
+  },
+  {
+    "revision": "8979281db4657873d6a22c0fd62af990",
+    "url": "/Gourab292.github.io/vapar/static/media/Museo700-Regular-webfont.8979281d.woff"
+  },
+  {
+    "revision": "0e22c44e12c9ba519400ea41a198ee80",
+    "url": "/Gourab292.github.io/vapar/static/media/Snowlake.0e22c44e.svg"
+  },
+  {
+    "revision": "4258e53b9b1d6556c5d311a200d1c910",
+    "url": "/Gourab292.github.io/vapar/static/media/Snowlake.4258e53b.woff"
+  },
+  {
+    "revision": "b987d36f3316ec057e96bae98178a942",
+    "url": "/Gourab292.github.io/vapar/static/media/Snowlake.b987d36f.ttf"
+  },
+  {
+    "revision": "e217b56860067821577fb6cbece86573",
+    "url": "/Gourab292.github.io/vapar/static/media/Snowlake.e217b568.eot"
+  },
+  {
+    "revision": "ec0698916f2ccec72d541e919554f295",
+    "url": "/Gourab292.github.io/vapar/static/media/blob1.ec069891.svg"
+  },
+  {
+    "revision": "64645d36a5863cb5b02dc0189ff4cac0",
+    "url": "/Gourab292.github.io/vapar/static/media/blob10.64645d36.svg"
+  },
+  {
+    "revision": "ce700191e37cd08f0b2de1482530fc43",
+    "url": "/Gourab292.github.io/vapar/static/media/blob11.ce700191.svg"
+  },
+  {
+    "revision": "73c0902bd9a3bff040fcab99293c623c",
+    "url": "/Gourab292.github.io/vapar/static/media/blob12.73c0902b.svg"
+  },
+  {
+    "revision": "5f29a25c51ca68a7ff4e133c963b9de7",
+    "url": "/Gourab292.github.io/vapar/static/media/blob13.5f29a25c.svg"
+  },
+  {
+    "revision": "3d791eb74219e5b0af9f40df992fdccf",
+    "url": "/Gourab292.github.io/vapar/static/media/blob14.3d791eb7.svg"
+  },
+  {
+    "revision": "0d7a79d8a56cd60f288e233b1ff07276",
+    "url": "/Gourab292.github.io/vapar/static/media/blob15.0d7a79d8.svg"
+  },
+  {
+    "revision": "216c94065f074ec9c93ec39aef4c36ed",
+    "url": "/Gourab292.github.io/vapar/static/media/blob2.216c9406.svg"
+  },
+  {
+    "revision": "e45703f1cf8db94ed31081eee3d0ed9e",
+    "url": "/Gourab292.github.io/vapar/static/media/blob3.e45703f1.svg"
+  },
+  {
+    "revision": "697a4813a4777ac9802a09cd8bdc2e69",
+    "url": "/Gourab292.github.io/vapar/static/media/blob4.697a4813.svg"
+  },
+  {
+    "revision": "8fb78ef83fce4eff54920fb5cec2222b",
+    "url": "/Gourab292.github.io/vapar/static/media/blob5.8fb78ef8.svg"
+  },
+  {
+    "revision": "f68a96c0d844bafd9e77b0c4c2093698",
+    "url": "/Gourab292.github.io/vapar/static/media/blob6.f68a96c0.svg"
+  },
+  {
+    "revision": "2d55e72785a8adcca4e40eb993476ba5",
+    "url": "/Gourab292.github.io/vapar/static/media/blob7.2d55e727.svg"
+  },
+  {
+    "revision": "0731accb5134646881aa709f5e417fcd",
+    "url": "/Gourab292.github.io/vapar/static/media/blob8.0731accb.svg"
+  },
+  {
+    "revision": "b79fa8519c6c00d186d81bad1bd8b594",
+    "url": "/Gourab292.github.io/vapar/static/media/blob9.b79fa851.svg"
+  },
+  {
+    "revision": "6fb412a0b0dad02888c911fd91a5e097",
+    "url": "/Gourab292.github.io/vapar/static/media/book.6fb412a0.svg"
+  },
+  {
+    "revision": "a8c874b93b3d848f39a71260c57e3863",
+    "url": "/Gourab292.github.io/vapar/static/media/closedhand.a8c874b9.cur"
+  },
+  {
+    "revision": "f811829aac035a9952624eb04b454031",
+    "url": "/Gourab292.github.io/vapar/static/media/cloudfiber.f811829a.svg"
+  },
+  {
+    "revision": "e672a7c90b94493df1805ff182898da9",
+    "url": "/Gourab292.github.io/vapar/static/media/concept14.e672a7c9.png"
+  },
+  {
+    "revision": "c23a4a5a3a29ac25a6f6b61fafa98d32",
+    "url": "/Gourab292.github.io/vapar/static/media/concept14@2x.c23a4a5a.png"
+  },
+  {
+    "revision": "72b76cd2cdedb0a1d39bbd191906432a",
+    "url": "/Gourab292.github.io/vapar/static/media/concept15.72b76cd2.png"
+  },
+  {
+    "revision": "e9923e4da7d216b9c281473fac6148ef",
+    "url": "/Gourab292.github.io/vapar/static/media/concept15@2x.e9923e4d.png"
+  },
+  {
+    "revision": "8c15bc18a82593d9a11ee996675ed540",
+    "url": "/Gourab292.github.io/vapar/static/media/concept18.8c15bc18.png"
+  },
+  {
+    "revision": "22baf29dd75fe07cbea2ed1d36c6008c",
+    "url": "/Gourab292.github.io/vapar/static/media/concept18@2x.22baf29d.png"
+  },
+  {
+    "revision": "06f1f082483f42672fcae2b18680d0b2",
+    "url": "/Gourab292.github.io/vapar/static/media/concept19.06f1f082.png"
+  },
+  {
+    "revision": "fe775753f24308cd1ee778c8efb61703",
+    "url": "/Gourab292.github.io/vapar/static/media/concept19@2x.fe775753.png"
+  },
+  {
+    "revision": "252449732e504be084655a725be45238",
+    "url": "/Gourab292.github.io/vapar/static/media/cpupath-w.25244973.svg"
+  },
+  {
+    "revision": "c8cf15e39d99d68140482a284bcdfd64",
+    "url": "/Gourab292.github.io/vapar/static/media/cpupath.c8cf15e3.svg"
+  },
+  {
+    "revision": "54a9f92b36905d7e93cf0e91f76900c8",
+    "url": "/Gourab292.github.io/vapar/static/media/custom.54a9f92b.woff"
+  },
+  {
+    "revision": "d1566868170b72fb5bf4828ef2b31dde",
+    "url": "/Gourab292.github.io/vapar/static/media/custom.d1566868.eot"
+  },
+  {
+    "revision": "eae9b6105ce54c6dd43df788e24c7a45",
+    "url": "/Gourab292.github.io/vapar/static/media/custom.eae9b610.svg"
+  },
+  {
+    "revision": "fe32d26402bb45f273f600ed5ac651d5",
+    "url": "/Gourab292.github.io/vapar/static/media/custom.fe32d264.ttf"
+  },
+  {
+    "revision": "bec6a7479036e1fd5ff2133e9785d986",
+    "url": "/Gourab292.github.io/vapar/static/media/email.bec6a747.svg"
+  },
+  {
+    "revision": "ef0a2aeb0058d2d6459bf9f4ba8873bc",
+    "url": "/Gourab292.github.io/vapar/static/media/emailopen.ef0a2aeb.svg"
+  },
+  {
+    "revision": "050375d5078f4a1e3fa78d6d9caf4cd2",
+    "url": "/Gourab292.github.io/vapar/static/media/exapath-w.050375d5.svg"
+  },
+  {
+    "revision": "ba045157abf470635cd8dbffe9fac4a7",
+    "url": "/Gourab292.github.io/vapar/static/media/exapath.ba045157.svg"
+  },
+  {
+    "revision": "5e787273c8ed134a94188fd339f50ee3",
+    "url": "/Gourab292.github.io/vapar/static/media/icofont.5e787273.svg"
+  },
+  {
+    "revision": "98110f08d0e8ce2ed7f931a944b6e116",
+    "url": "/Gourab292.github.io/vapar/static/media/icofont.98110f08.ttf"
+  },
+  {
+    "revision": "a48e80b019cd2cb63efdc928691a3a0a",
+    "url": "/Gourab292.github.io/vapar/static/media/icofont.a48e80b0.eot"
+  },
+  {
+    "revision": "d4f609b06a7b5ef0d165a67068700f96",
+    "url": "/Gourab292.github.io/vapar/static/media/icofont.d4f609b0.woff"
+  },
+  {
+    "revision": "f6cd6052a8f6e21786f605d5c35e7864",
+    "url": "/Gourab292.github.io/vapar/static/media/livechat.f6cd6052.svg"
+  },
+  {
+    "revision": "f2a70cd3811f71c4f0ba80b4961db9e8",
+    "url": "/Gourab292.github.io/vapar/static/media/lock.f2a70cd3.svg"
+  },
+  {
+    "revision": "0029f1b43b8f1406a799a66546eceefa",
+    "url": "/Gourab292.github.io/vapar/static/media/motpath-w.0029f1b4.svg"
+  },
+  {
+    "revision": "0c3f9e4ee896cd3df4b92a71d3978645",
+    "url": "/Gourab292.github.io/vapar/static/media/motpath.0c3f9e4e.svg"
+  },
+  {
+    "revision": "b06c243f534d9c5461d16528156cd5a8",
+    "url": "/Gourab292.github.io/vapar/static/media/openhand.b06c243f.cur"
+  },
+  {
+    "revision": "6b4c072afa592ce4ecbba435ba34a3bc",
+    "url": "/Gourab292.github.io/vapar/static/media/phone.6b4c072a.svg"
+  },
+  {
+    "revision": "fbe5133d2472ec5e86f77917f20575e6",
+    "url": "/Gourab292.github.io/vapar/static/media/promo-w.fbe5133d.svg"
+  },
+  {
+    "revision": "57c1b70cbee1b08627e2fb8126d52109",
+    "url": "/Gourab292.github.io/vapar/static/media/promopath.57c1b70c.svg"
+  },
+  {
+    "revision": "8ed333d33cffddeef2f258737ebb3bab",
+    "url": "/Gourab292.github.io/vapar/static/media/question.8ed333d3.svg"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/Gourab292.github.io/vapar/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "1142a254da20b59e7d10ac39e78c4fbf",
+    "url": "/Gourab292.github.io/vapar/static/media/revicons.1142a254.svg"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/Gourab292.github.io/vapar/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/Gourab292.github.io/vapar/static/media/revicons.2feb69cc.eot"
+  },
+  {
+    "revision": "f2c149c23f8167079119f985795a50e4",
+    "url": "/Gourab292.github.io/vapar/static/media/topbanner01.f2c149c2.jpg"
+  },
+  {
+    "revision": "f2c149c23f8167079119f985795a50e4",
+    "url": "/Gourab292.github.io/vapar/static/media/topbanner02.f2c149c2.jpg"
+  },
+  {
+    "revision": "f2c149c23f8167079119f985795a50e4",
+    "url": "/Gourab292.github.io/vapar/static/media/topbanner03.f2c149c2.jpg"
+  },
+  {
+    "revision": "4aebd068295b16288f3aee4f2caa3859",
+    "url": "/Gourab292.github.io/vapar/static/media/topbanner04.4aebd068.jpg"
+  },
+  {
+    "revision": "4aebd068295b16288f3aee4f2caa3859",
+    "url": "/Gourab292.github.io/vapar/static/media/topbanner05.4aebd068.jpg"
+  },
+  {
+    "revision": "4aebd068295b16288f3aee4f2caa3859",
+    "url": "/Gourab292.github.io/vapar/static/media/topbanner06.4aebd068.jpg"
+  },
+  {
+    "revision": "4aebd068295b16288f3aee4f2caa3859",
+    "url": "/Gourab292.github.io/vapar/static/media/topbanner07.4aebd068.jpg"
+  },
+  {
+    "revision": "f2c149c23f8167079119f985795a50e4",
+    "url": "/Gourab292.github.io/vapar/static/media/topbanner08.f2c149c2.jpg"
+  },
+  {
+    "revision": "f2c149c23f8167079119f985795a50e4",
+    "url": "/Gourab292.github.io/vapar/static/media/topbanner09.f2c149c2.jpg"
+  },
+  {
+    "revision": "f2c149c23f8167079119f985795a50e4",
+    "url": "/Gourab292.github.io/vapar/static/media/topbanner10.f2c149c2.jpg"
+  },
+  {
+    "revision": "f2c149c23f8167079119f985795a50e4",
+    "url": "/Gourab292.github.io/vapar/static/media/topbanner11.f2c149c2.jpg"
+  },
+  {
+    "revision": "f2c149c23f8167079119f985795a50e4",
+    "url": "/Gourab292.github.io/vapar/static/media/topbanner12.f2c149c2.jpg"
+  },
+  {
+    "revision": "f2c149c23f8167079119f985795a50e4",
+    "url": "/Gourab292.github.io/vapar/static/media/topbanner13.f2c149c2.jpg"
+  },
+  {
+    "revision": "f2c149c23f8167079119f985795a50e4",
+    "url": "/Gourab292.github.io/vapar/static/media/topbanner14.f2c149c2.jpg"
+  },
+  {
+    "revision": "f2c149c23f8167079119f985795a50e4",
+    "url": "/Gourab292.github.io/vapar/static/media/topbanner15.f2c149c2.jpg"
+  },
+  {
+    "revision": "f2c149c23f8167079119f985795a50e4",
+    "url": "/Gourab292.github.io/vapar/static/media/topbanner16.f2c149c2.jpg"
+  }
+]);
---
+++ static/js/0.30e19712.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpstartup=this.webpackJsonpstartup||[]).push([[0],{40:function(p,s,t){}}]);
+//# sourceMappingURL=0.30e19712.chunk.js.map
